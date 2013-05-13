f7d6c06ecf10c29b777a953b5f98dbaeb2882e37
Matthew McPherrin <matthew@mcpherrin.ca>
2013-05-13 12:30:49 -0400

    tetris

--- tetris/game.js
+++ tetris/game.js
@@ -2,10 +2,11 @@
 
 // pfft seperation of concerns.  I'm one man, it's all my concern.
 var board;
-var boardelt;
-var active;
+var boardElt;
 var score;
 var piece;
+var pieceRow;
+var pieceCol;
 var height = 16;
 var width = 10;
 var stopflag = false;
@@ -38,6 +39,22 @@ function setup() {
                                 window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
   })();
+
+  document.onkeyup = function(event) {
+    var key = (event || window.event).keyCode;
+    if(key == 37) { // left
+      if(pieceCol > 0) {
+        pieceCol -= 1;
+      }
+    } else if(key == 39) { // right
+      if((pieceCol + pieces[piece][0].length) < width) {
+       pieceCol += 1;
+      }
+    } else {
+      return;
+    }
+    renderboard();
+  }
 }
 
 // Start running the game loop
@@ -46,15 +63,19 @@ function start() {
   console.log("start");
   // New board:
   board=[];
-  boardelt = document.getElementById('container');
+  boardElt = document.getElementById('container');
+  boardElt.innerHTML="";
   for(var row = 0; row < height; row++){
     board[row] = [];
+    var docRow = document.createElement('div');
+    boardElt.appendChild(docRow);
     for(var col = 0; col < width; col++) {
       board[row][col] = 0;
+      docRow.appendChild(document.createElement('span'));
     }
   }
 
-  piece = randompiece();
+  newactive();
 
   score = 0;
   window.setTimeout(tick, 1000);
@@ -78,30 +99,60 @@ function tick() {
   // TODO: set tick rate based on game level
   if(stopflag == true) {
     stopflag = false;
+    console.log("GAME OVER");
   } else {
     window.setTimeout(tick, 1000);
   }
+  renderboard();
 }
 
 // Does every nonzero element of the current piece have a
 // zero below it in the board, at its current height?
 function candrop() {
-  return true;
+  if(pieceRow > 0) {
+    var p = pieces[piece];
+    for(var i = 0; i < p.length; i++) {
+      for(var j = 0; j < p[i].length; j++) {
+        if(p[i][j] !== 0) {
+          if(board[pieceRow-1 + i][pieceCol + j]) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+  return false;
 }
 
 // Move the active piece down a row
 function movedown() {
+  pieceRow -= 1;
 }
 
 function finishpiece() {
   mergeactive();
   clearrows();
   testendgame();
+  newactive();
 }
 
 // Merge the active piece into the board
 function mergeactive() {
+  var p = pieces[piece];
+  for(var i = 0; i < p.length; i++) {
+    for(var j = 0; j < p[i].length; j++) {
+      if(p[i][j] !== 0) {
+        board[pieceRow + i][pieceCol + j] = piece;
+      }
+    }
+  }
+}
 
+function newactive() {
+  piece = randompiece();
+  pieceRow = height-(pieces[piece].length);
+  pieceCol = width/2 - Math.ceil(pieces[piece][0].length/2);
 }
 
 // Find rows that are full, eliminate, and move down.
@@ -134,6 +185,7 @@ function testendgame() {
       if(board[height-i][j] !== 0) {
         // maybe something more here:
         stop();
+        console.log("GAME OVUH");
       }
     }
   }
@@ -144,4 +196,31 @@ function testendgame() {
 function randompiece() {
   var pns = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
   return pns[Math.floor(Math.random()*pns.length)];
+}
+
+function renderboard() {
+  var boardRows = boardElt.children
+  var l = boardRows.length;
+
+  for(var r = 0; r < height; r++) {
+    for(var col = 0; col < width; col++) {
+      var it = board[r][col];
+      var clss;
+      if(it !== 0) {
+        clss = it;
+      } else {
+        clss = "";
+      }
+      boardRows[l - r - 1].children[col].className = clss;
+    }
+  }
+  // Render the active piece:
+  var p = pieces[piece];
+  for(var i = 0; i < p.length; i++) {
+    for(var j = 0; j < p[i].length; j++) {
+      if(p[i][j] !== 0) {
+        boardRows[l - i - 1 - pieceRow].children[j + pieceCol].className = piece;
+      }
+    }
+  }
 }
